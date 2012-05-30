325b55f417f6224417e1bb8f4984c46fb24bd6c2
Marc Abramowitz <marc@marc-abramowitz.com>
2012-05-30 11:23:21 -0700

    Add support for tox (http://tox.testrun.org/)

--- tests/test_skeletor.py
+++ tests/test_skeletor.py
@@ -5,5 +5,5 @@ import unittest
 class SkeletorTestCase(unittest.TestCase):
 
     def test_skeletor_help(self):
-        first_line = subprocess.check_output(["skeletor", "--help"]).splitlines()[0]
+        first_line = subprocess.check_output(["python", "skeletor.py", "--help"]).splitlines()[0].decode('utf-8')
         assert 'usage: skeletor' in first_line, "skeletor --help output's first line contains 'usage: skeletor'"
